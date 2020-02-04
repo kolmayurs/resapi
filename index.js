3880af16613bc75d971658f7db0dade1e0b3a626
Mayur Koli <kolimayurs@gmail.com>
2020-02-04 12:05:38 +0530

    commit

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://kolimayurs:29031991@cluster0-onizc.mongodb.net';
 const allowCrossDomain = function(req, res, next) {
-   res.header('Access-Control-Allow-Origin', 'https://amp.gmail.dev');
+    res.header('Access-Control-Allow-Origin', 'https://amp.gmail.dev');
     res.header('AMP-Access-Control-Allow-Source-Origin', 'amp@gmail.dev');
     res.header('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
-    res.header('status', '200');
-
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
-      res.status(200)
-    }
-    else {
-      next();
+        res.status(200)
+    } else {
+        next();
     }
 };
 app.use(allowCrossDomain);
@@ -22,7 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/subscribe', (req, res) => {
-	
+    res.header("Content-type: application/json");
+    res.header("Access-Control-Allow-Credentials: true");
+    res.header('Access-Control-Allow-Origin: *');
+
     MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
         if (err) {
             return res.send(err);
@@ -42,6 +42,9 @@ app.post('/subscribe', (req, res) => {
 
 
 app.get('/emails', (req, res) => {
+    res.header("Content-type: application/json");
+    res.header("Access-Control-Allow-Credentials: true");
+    res.header('Access-Control-Allow-Origin: *');
     MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
         if (err) {
             return res.send(err);
@@ -49,7 +52,7 @@ app.get('/emails', (req, res) => {
         var dbo = db.db("myDB");
         dbo.collection("resapi").find({}).toArray(function(err, result) {
             if (err) throw err;
-            res.status(200).json({"items": result});
+            res.status(200).json({ "items": result });
             db.close();
         });
     });
@@ -57,12 +60,15 @@ app.get('/emails', (req, res) => {
 
 
 app.post('/add', (req, res) => {
+    res.header("Content-type: application/json");
+    res.header("Access-Control-Allow-Credentials: true");
+    res.header('Access-Control-Allow-Origin: *');
     MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
         if (err) {
             return res.send(err);
         }
         var dbo = db.db("myDB");
-        var myobj = { name:req.body.name, email: req.body.email };
+        var myobj = { name: req.body.name, email: req.body.email };
         dbo.collection("amp-data").insertOne(myobj, function(err, result) {
             if (err) {
                 return res.send(err);
@@ -75,6 +81,9 @@ app.post('/add', (req, res) => {
 });
 
 app.get('/data', (req, res) => {
+    res.header("Content-type: application/json");
+    res.header("Access-Control-Allow-Credentials: true");
+    res.header('Access-Control-Allow-Origin: *');
     MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
         if (err) {
             return res.send(err);
@@ -83,7 +92,7 @@ app.get('/data', (req, res) => {
         dbo.collection("amp-data").find({}).toArray(function(err, result) {
 
             if (err) throw err;
-            res.status(200).json({"items": result});
+            res.status(200).json({ "items": result });
             db.close();
         });
     });
